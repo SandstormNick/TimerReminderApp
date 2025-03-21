9107e83d1476c7176770868caa35ba4835cc3d40
SandstormNick <gibbensnick@gmail.com>
2025-03-21 15:32:05 +0200

    Add all controls to form

--- TimerReminderApp/TimeReminderForm.Designer.cs
+++ TimerReminderApp/TimeReminderForm.Designer.cs
@@ -28,18 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtReminderDescription = new TextBox();
+            nudHours = new NumericUpDown();
+            nudMinutes = new NumericUpDown();
+            lblHours = new Label();
+            lblMinutes = new Label();
+            btnStartCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)nudHours).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudMinutes).BeginInit();
             SuspendLayout();
+            // 
+            // txtReminderDescription
+            // 
+            txtReminderDescription.Location = new Point(12, 12);
+            txtReminderDescription.Name = "txtReminderDescription";
+            txtReminderDescription.Size = new Size(386, 27);
+            txtReminderDescription.TabIndex = 0;
+            // 
+            // nudHours
+            // 
+            nudHours.Location = new Point(104, 58);
+            nudHours.Maximum = new decimal(new int[] { 24, 0, 0, 0 });
+            nudHours.Name = "nudHours";
+            nudHours.Size = new Size(75, 27);
+            nudHours.TabIndex = 1;
+            // 
+            // nudMinutes
+            // 
+            nudMinutes.Location = new Point(104, 102);
+            nudMinutes.Maximum = new decimal(new int[] { 59, 0, 0, 0 });
+            nudMinutes.Name = "nudMinutes";
+            nudMinutes.Size = new Size(75, 27);
+            nudMinutes.TabIndex = 2;
+            // 
+            // lblHours
+            // 
+            lblHours.AutoSize = true;
+            lblHours.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblHours.Location = new Point(12, 53);
+            lblHours.Name = "lblHours";
+            lblHours.Size = new Size(68, 28);
+            lblHours.TabIndex = 3;
+            lblHours.Text = "Hours:";
+            // 
+            // lblMinutes
+            // 
+            lblMinutes.AutoSize = true;
+            lblMinutes.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMinutes.Location = new Point(12, 97);
+            lblMinutes.Name = "lblMinutes";
+            lblMinutes.Size = new Size(86, 28);
+            lblMinutes.TabIndex = 4;
+            lblMinutes.Text = "Minutes:";
+            // 
+            // btnStartCancel
+            // 
+            btnStartCancel.Location = new Point(12, 146);
+            btnStartCancel.Name = "btnStartCancel";
+            btnStartCancel.Size = new Size(386, 29);
+            btnStartCancel.TabIndex = 5;
+            btnStartCancel.Text = "Start";
+            btnStartCancel.UseVisualStyleBackColor = true;
             // 
             // TimeReminderForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(491, 340);
+            ClientSize = new Size(410, 192);
+            Controls.Add(btnStartCancel);
+            Controls.Add(lblMinutes);
+            Controls.Add(lblHours);
+            Controls.Add(nudMinutes);
+            Controls.Add(nudHours);
+            Controls.Add(txtReminderDescription);
             Name = "TimeReminderForm";
-            Text = "Form1";
+            Text = "Timer Reminder";
+            ((System.ComponentModel.ISupportInitialize)nudHours).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudMinutes).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtReminderDescription;
+        private NumericUpDown nudHours;
+        private NumericUpDown nudMinutes;
+        private Label lblHours;
+        private Label lblMinutes;
+        private Button btnStartCancel;
     }
 }
